4dbab7f2dd96c9fd4e72ea7271d89a79726668b1
manucorcos <manucorcos@manucorcos-VirtualBox>
2019-12-04 16:25:47 -0300

    arrancando

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: manucorcos
+ *
+ * Created on December 4, 2019, 4:21 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
